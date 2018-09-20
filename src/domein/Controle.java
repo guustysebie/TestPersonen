80097010319fa35bf73ac92d801b303efa3b044c
Niek Gasthuys <KriNi@172.18.144.78>
2018-09-20 12:49:52 +0200

    testen op fouten met safpauze

--- src/domein/Controle.java
+++ src/domein/Controle.java
@@ -1,12 +1,30 @@
 package domein;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Controle {
 
+  
+    
+  
+    private static final Pattern patroonGeenCijfers = Pattern.compile("[0-9]");
+    private static final Pattern patroonGeenLetters = Pattern.compile("[a-zA-Z]");
 
+
+     private static boolean bevatTekstLetters(String gsm) {
+         //controleert of een gsm geen letters bevat
+         return patroonGeenLetters.matcher(gsm).matches();
+     }
+    private static boolean bevatTekstCijfers(String tekst) {
+        //controleert of de naam cijfers bevat en geeft true terug als dat zo is
+        return patroonGeenCijfers.matcher(tekst).matches();
+    }
+    
     protected static void controleerNaam(String naam) {
+       
         if (naam == null){
             throw  new IllegalArgumentException("naam mag niet null zijn ");
         }
@@ -16,20 +34,38 @@ public class Controle {
         if (naam.length() > 45){
             throw  new IllegalArgumentException("naam mag niet langer zijn dan 45");
         }
-
-        if (naam.contains("1")){
-            // moet alle cijfers zijn
-            if (naam.length() == 0){
-                throw  new IllegalArgumentException("naam mag geen cijfers bevatten");
-            }
+        if(bevatTekstCijfers(naam)){
+       throw  new IllegalArgumentException("de naam mag geen cijfers bevatten");
         }
-
-
-
     }
 
     protected static void controleerGSM(String gsm){
-
+        String hulpgsm = gsm;
+        
+         if(gsm == null)
+       {
+           throw new IllegalArgumentException("gsm mag niet null zijn");
+       }
+       if(gsm.length()==0)
+       {
+           throw new IllegalArgumentException("gsm mag niet leeg zijn");
+       }
+       if(gsm.length()>13)
+       {
+           throw new IllegalArgumentException
+       }
+      if(bevatTekstLetters(gsm))
+      {
+          throw new IllegalArgumentException("gsm mag geen letters bevatten");
+      }
+      
+      String eersteVierCijfersGsm = gsm.substring(0, 3);
+      
+      if(!eersteVierCijfersGsm.equals("0032"))
+      {
+          throw new IllegalArgumentException("gsm nummer moet beginnen met 0032");
+      }
+      gsm=hulpgsm;
     }
 
     protected static  void controleerEmail(String email){
